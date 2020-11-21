6c9a02e98b072d4a2b34c91374ec7f59975b97df
SaudFin <developer.saud@gmail.com>
2020-11-21 18:14:27 +0300

    POST & GET are working

--- server.js
+++ server.js
@@ -20,6 +20,28 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static("website"));
 
+// Routes
+
+// GET Route
+app.get("/", function(req, res) {
+  res.send(projectData);
+});
+
+// POST Route
+app.post("/add", function(req, res) {
+  let data = req.body;
+  console.log("here");
+  //   to specify the data that we wanted to add to our endpoint
+  // let entry = {
+  // temperature: data.temperature,
+  // date: data.date,
+  // userResponse: data.userResponse
+  // };
+  projectData.temp = data.temperature;
+  projectData.date = data.date;
+  projectData.userResponse = data.userResponse;
+  console.log(projectData);
+});
 // Setup Server
-const port = 5000;
-const server = app.listen(port, () => console.log("it's working"));
+const port = 8000;
+const server = app.listen(port, () => console.log(`it is running on ${port}`));
--- website/app.js
+++ website/app.js
@@ -1,5 +1,49 @@
 /* Global Variables */
-
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = "f2ee3a4ba3d60f00652cb1d048a6642b";
+const generate = document.getElementById("generate");
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+// 94040&appid=f2ee3a4ba3d60f00652cb1d048a6642b
+// async GET function
+const getWeather = async (baseURL, zipCode, apiKey) => {
+  console.log(`${baseURL}${zipCode}${apiKey}`);
+  const res = await fetch(`${baseURL}${zipCode}&appid=${apiKey}`);
+  const data = await res.json();
+  return data;
+};
+
+//  async POST function
+const postLocation = async (url = "", data = {}) => {
+  const res = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  try {
+    console.log(res);
+    const newData = await res;
+    // const newData = await res.json();
+    console.log(newData);
+    return newData;
+  } catch (err) {
+    console.log("error here", err);
+  }
+};
+
+generate.addEventListener("click", () => {
+  let zipCode = document.getElementById("zip").value;
+  getWeather(baseURL, zipCode, apiKey).then(function(data) {
+    // console.log(data);
+    postLocation("/add", {
+      temperature: data.main.temp,
+      date: newDate,
+      userResponse: document.getElementById("feelings").value
+    });
+  });
+});
